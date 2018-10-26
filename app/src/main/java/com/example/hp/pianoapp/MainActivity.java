ad98b50ae5745d1c3789339ce24992b0ff1fe107
areeshaFatima <areeshaf7@gmail.com>
2018-10-27 01:47:40 +0530

    basic

--- app/src/main/java/com/example/hp/pianoapp/MainActivity.java
+++ app/src/main/java/com/example/hp/pianoapp/MainActivity.java
@@ -1,13 +1,63 @@
 package com.example.hp.pianoapp;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+
+
+    public void playTonea(View view){
+
+        mediaPlayer=MediaPlayer.create(this,R.raw.a);
+        mediaPlayer.start();
+
+    }
+    public void playToneb(View view){
+
+        mediaPlayer=MediaPlayer.create(this,R.raw.b);
+        mediaPlayer.start();
+
+    }
+    public void playTonec(View view){
+
+        mediaPlayer=MediaPlayer.create(this,R.raw.c);
+        mediaPlayer.start();
+
+    }
+    public void playToned(View view){
+
+        mediaPlayer=MediaPlayer.create(this,R.raw.d);
+        mediaPlayer.start();
+
+    }
+    public void playTonee(View view){
+
+        mediaPlayer=MediaPlayer.create(this,R.raw.e);
+        mediaPlayer.start();
+
+    }
+    public void playTonef(View view){
+
+        mediaPlayer=MediaPlayer.create(this,R.raw.f);
+        mediaPlayer.start();
+
+    }
+    public void playToneg(View view){
+
+        mediaPlayer=MediaPlayer.create(this,R.raw.g);
+        mediaPlayer.start();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
